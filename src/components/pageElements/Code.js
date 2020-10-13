26dd07fc42d77b78f41672049fae1c779f1683e9
Diogo Lessa <diogolessacosta@gmail.com>
2020-10-13 08:17:39 -0300

    add copy to clipboard button with action

--- src/components/pageElements/Code.js
+++ src/components/pageElements/Code.js
@@ -2,8 +2,10 @@ import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
 import React from "react";
 import styled from "styled-components";
+import { copyToClipboard } from "../../utils/copy-to-clipboard";
 
 const Pre = styled.pre`
+  position: relative;
   text-align: left;
   margin: 1em 0;
   padding: 0.5em;
@@ -23,19 +25,38 @@ const LineNo = styled.span`
   opacity: 0.3;
 `;
 
-export const Code = ({ lang = "", code }) => (
-  <Highlight {...defaultProps} code={code} language={lang} theme={theme}>
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <Pre className={className} style={style}>
-        {tokens.map((line, i) => (
-          <div {...getLineProps({ line, key: i })}>
-            <LineNo>{i + 1}</LineNo>
-            {line.map((token, key) => (
-              <span {...getTokenProps({ token, key })} />
-            ))}
-          </div>
-        ))}
-      </Pre>
-    )}
-  </Highlight>
-);
+const CopyCode = styled.button`
+  position: absolute;
+  right: 0.25rem;
+  border: 0;
+  border-radius: 3px;
+  margin: 0.25rem;
+  opacity: 0.3;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const Code = ({ lang = "", code }) => {
+  const onCopyClick = () => {
+    copyToClipboard(code);
+  };
+
+  return (
+    <Highlight {...defaultProps} code={code} language={lang} theme={theme}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          <CopyCode onClick={onCopyClick}>Copy</CopyCode>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  );
+};
