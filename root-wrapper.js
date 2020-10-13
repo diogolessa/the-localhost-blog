76e92284c3abaa50a32a2b94641279ad14883e81
Diogo Lessa <diogolessacosta@gmail.com>
2020-10-12 22:40:04 -0300

    use Highlight for code blocks

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,4 +1,6 @@
 import { MDXProvider } from "@mdx-js/react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
 import React from "react";
 
 const components = {
@@ -6,6 +8,31 @@ const components = {
   "p.inlineCode": (props) => (
     <code {...props} style={{ backgroundColor: "lightgray" }}></code>
   ),
+  pre: (props) => {
+    const className = props.children.props.className;
+    const matches = className.match(/language-(?<lang>.*)/);
+
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children.trim()}
+        language={matches?.groups?.lang || ""}
+        theme={theme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    );
+  },
 };
 
 export const wrapRootElement = ({ element }) => {
