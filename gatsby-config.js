0ce7972a8677a843af115a594290e030e155bca0
Diogo Lessa <diogolessacosta@gmail.com>
2020-10-14 08:31:49 -0300

    add and configure image packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,38 @@
 module.exports = {
   siteMetadata: {
-    title: 'The localhost blog',
-    description: 'This is my coding blog'
+    title: "The localhost blog",
+    description: "This is my coding blog",
   },
   plugins: [
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-mdx',
+      resolve: "gatsby-plugin-mdx",
       options: {
-        extensions: ['.mdx', '.md'],
+        extensions: [".mdx", ".md"],
       },
+      gatsbyRemarkPlugins: [
+        {
+          resolve: "gatsby-remark-images",
+          options: {
+            maxWidth: 540,
+          },
+        },
+      ],
+      plugin: [
+        {
+          resolve: "gatsby-remark-images",
+          options: {
+            maxWidth: 540,
+          },
+        },
+      ],
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/posts`,
-        name: 'posts'
+        name: "posts",
       },
     },
   ],
