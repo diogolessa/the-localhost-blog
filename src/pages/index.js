d3e721963397d790ff68567dfe13ea75b5ba66c9
Diogo Lessa <diogolessacosta@gmail.com>
2020-10-14 08:32:07 -0300

    add  image query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
 import React from "react";
 import styled from "styled-components";
 import { Layout } from "../components/Layout";
@@ -7,6 +8,10 @@ const IndexWrapper = styled.main``;
 
 const PostWrapper = styled.div``;
 
+const Image = styled(Img)`
+  border-radius: 5px;
+`;
+
 export default ({ data }) => {
   return (
     <Layout>
@@ -14,6 +19,9 @@ export default ({ data }) => {
         {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
           <PostWrapper key={id}>
             <Link to={fields.slug}>
+              {!!frontmatter.cover ? (
+                <Image sizes={frontmatter.cover.childImageSharp.sizes} />
+              ) : null}
               <h1>{frontmatter.title}</h1>
               <p>{frontmatter.date}</p>
               <p>{excerpt}</p>
@@ -36,7 +44,15 @@ export const query = graphql`
         excerpt(pruneLength: 250)
         frontmatter {
           title
-          date
+          date(formatString: "YYYY MM DD")
+          cover {
+            publicURL
+            childImageSharp {
+              sizes(maxWidth: 2000, traceSVG: { color: "#639" }) {
+                ...GatsbyImageSharpSizes_tracedSVG
+              }
+            }
+          }
         }
         fields {
           slug
